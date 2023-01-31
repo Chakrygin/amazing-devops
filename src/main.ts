07989df741395abb52229e8d2a09aaf987d6d85d
Igor Chakrygin <igor@chakrygin.ru>
2023-02-01 00:24:26 +0300

    Add KubernetesScraper

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { App } from 'core';
 
 import DevBlogsScraper from './scrapers/DevBlogsScraper'
 import HabrScraper from './scrapers/HabrScraper'
+import KubernetesScraper from './scrapers/KubernetesScraper'
 
 const app = new App(() => [
   new DevBlogsScraper('commandline'),
   new DevBlogsScraper('powershell'),
   new HabrScraper('devops'),
   new HabrScraper('kubernetes'),
+  new KubernetesScraper(),
 ]);
 
 void app.run();
